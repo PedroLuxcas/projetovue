43b91f42b8b6bc3c562aeef23f0388d5d78abd35
Pedro Lucas <pedro.lcnascimento@outlook.com>
2023-11-22 09:27:09 -0300

    first commit

---
+++ src/components/Message.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="massege-container">
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Message",
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+.massege-container {
+    color: #004085;
+    background-color: #cce5ff;
+    border: 2px solid #b8daff;
+    border-radius: 5px;
+    padding: 10px;
+    max-width: 400px;
+    margin: 30px auto;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Banner />
+  <div class="main-container">
+     <h1>Monte o seu burger:</h1>
+     <BurgerForm />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+   import Banner from "../components/Banner.vue"
+   import BurgerForm from "../components/BurgerForm.vue"
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+   export default {
+    nome: "Home",
+    components: {
+      Banner,
+      BurgerForm
+    }
+   }
 </script>
